f12799223221eabfc2ec56bc6ccb414e1bf03cdc
Joel S <joelscaria@gmail.com>
2015-03-23 09:39:57 -0700

    Update js-sample.js

--- js-sample.js
+++ js-sample.js
@@ -133,3 +133,38 @@ function clearSaverLog()
 {
     document.getElementById('saverConsole').innerHTML = "";
 }
+
+var html = "";
+html += "<div class='row'>";
+html += "<span class='cell heading'>Options</span>";
+html += "<span class='cell'>";
+html += "<input id='multiSelect' type='checkbox' name='multiSelect' />";
+html += "<label>Enable multiselect</label>";
+html += "</span>";
+html += "</div>";
+html += "<div class='row'>";
+html += "<span class='cell heading'>Link Type</span>";
+html += "<span class='cell'>";
+html += "<input id='linkType' type='radio' value='webViewLink' name='linkType' checked='checked' />";
+html += "<label>Web View Link</label>";
+html += "<input id='linkType' type='radio' value='downloadLink' name='linkType' />";
+html += "<label>Download link</label>";
+html += "</span>";
+html += "</div>";
+html += "<div class='row'>";
+html += "<span class='cell heading'>Button</span>";
+html += "<span class='cell'>";
+html += "<button onClick='javascript:launchOneDrivePicker();' title='Open from OneDrive' style='direction: ltr; border: 1px solid rgb(9, 74, 178); height: 20px; padding-left: 4px; padding-right: 4px; text-align: center; cursor: pointer; background-color: rgb(9, 74, 178);'>";
+html += "<img src='https://js.live.net/v5.0/images/SkyDrivePicker/SkyDriveIcon_white.png' style='vertical-align: middle; height: 16px;'>";
+html += "<span style='font-family: 'Segoe UI', 'Segoe UI Web Regular', 'Helvetica Neue', 'BBAlpha Sans', 'S60 Sans', Arial, sans-serif; font-size: 12px; font-weight: bold; color: rgb(255, 255, 255); text-align: center; vertical-align: middle; margin-left: 2px; margin-right: 0px;'>Open from OneDrive</span>";
+html += "</button>";
+html += "</span>";
+html += "</div>";
+html += "<div class='row'>";
+html += "<span class='cell heading' style='vertical-align: top;'>Result</span>";
+html += "<span class='cell'>";
+html += "<div id='pickerConsole' class='console'>Pick a file to OneDrive to see the return result</div>";
+html += "</span>";
+html += "</div>";
+
+document.getElementById("pickerTable").innerHTML = html;
